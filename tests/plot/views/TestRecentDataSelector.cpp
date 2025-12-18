5489effb79acf0ca7916ca14824423b44d085bba
vince-smdt <vince.smdt@gmail.com>
2025-12-17 19:26:37 -0500

    Fix TestRecentDataSelector tests

--- tests/plot/views/TestRecentDataSelector.cpp
+++ tests/plot/views/TestRecentDataSelector.cpp
@@ -2,26 +2,31 @@
 
 #include <doctest.h>
 
+// TODO: Add tests for when eraseOld, clear and compress PlotData methods are called
+
 TEST_CASE("RecentDataSelector should correctly select data") {
     SUBCASE("Window size larger than 1") {
+        PlotData data;
         std::vector<float> timeline = {0.0f, 1000.0f, 2000.0f, 3000.0f, 4000.0f, 5000.0f};
-        RecentDataSelector selector(2500);
+        RecentDataSelector selector(&data, 2500);
         RecentDataSelector::Window window = selector.getWindow(timeline);
         CHECK(window.start == 3);
         CHECK(window.size == 3);
     }
 
     SUBCASE("Window size equal to 1") {
+        PlotData data;
         std::vector<float> timeline = {0.0f, 1000.0f, 2000.0f, 3000.0f, 4000.0f, 5000.0f, 8000.f, 10000.f};
-        RecentDataSelector selector(500);
+        RecentDataSelector selector(&data, 500);
         RecentDataSelector::Window window = selector.getWindow(timeline);
         CHECK(window.start == 7);
         CHECK(window.size == 1);
     }
 
     SUBCASE("Window size same as input size") {
+        PlotData data;
         std::vector<float> timeline = {0.0f, 1000.0f, 2000.0f, 3000.0f, 4000.0f, 5000.0f, 8000.f, 10000.f};
-        RecentDataSelector selector(15000);
+        RecentDataSelector selector(&data, 15000);
         RecentDataSelector::Window window = selector.getWindow(timeline);
         CHECK(window.start == 0);
         CHECK(window.size == 8);
