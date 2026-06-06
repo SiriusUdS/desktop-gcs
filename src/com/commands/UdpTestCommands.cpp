e8861995ffa732772600fde54f5addf5f9d74f04
XavBell <56218769+XavBell@users.noreply.github.com>
2026-06-06 18:47:36 -0400

    fix mineur

--- src/com/commands/UdpTestCommands.cpp
+++ src/com/commands/UdpTestCommands.cpp
@@ -8,19 +8,21 @@
 #include <ctime>
 
 void UdpTestCommands::testValve(uint16_t valueToSend) {
-    ServoPacket servoPacket;
+    ServoPacket* servoPacket = new ServoPacket();
     
-    servoPacket.frame.status = SERVO_MANUAL; 
-    servoPacket.frame.reserved = 0; 
-    servoPacket.frame.value = valueToSend;
+    servoPacket->frame.status = SERVO_MANUAL; 
+    servoPacket->frame.reserved = 0; 
+    servoPacket->frame.value = valueToSend;
     
-    servoPacket.frame.header.frame.deviceId = 0x01; // Example device ID
-    servoPacket.frame.header.frame.payloadId = SET_SERVO; // Command to set servo
-    servoPacket.frame.header.frame.payloadLength = sizeof(servoPacket.data) - sizeof(servoPacket.frame.crc);
-    servoPacket.frame.header.frame.deviceState = 0; // Example device state
-    servoPacket.frame.header.frame.reserved = 0;
-    servoPacket.frame.header.frame.deviceTsMs = 0; //Example
+    servoPacket->frame.header.frame.deviceId = 0x01; // Example device ID
+    servoPacket->frame.header.frame.payloadId = SET_SERVO; // Command to set servo
+    servoPacket->frame.header.frame.payloadLength = sizeof(servoPacket->data) - sizeof(servoPacket->frame.crc);
+    servoPacket->frame.header.frame.deviceState = 0; // Example device state
+    servoPacket->frame.header.frame.reserved = 0;
+    servoPacket->frame.header.frame.deviceTsMs = 0; //Example
     
-    servoPacket.frame.crc = CRC::computeCrcUDP(servoPacket.data, sizeof(servoPacket.data) - sizeof(servoPacket.frame.crc));
-    ComTask::com->write(servoPacket.data);
+    servoPacket->frame.crc = CRC::computeCrcUDP(servoPacket->data, sizeof(servoPacket->data) - sizeof(servoPacket->frame.crc));
+    ComTask::com->write(servoPacket->data);
+    
+    delete servoPacket;
 }
